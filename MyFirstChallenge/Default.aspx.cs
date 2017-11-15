4a1e5305e6020722d25ded314fb4e338b58b2008
Travis Brooks <tbrooks3141@gmail.com>
2017-11-15 15:32:21 -0800

    First commit.

---
+++ MyFirstChallenge/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void fortuneButton_Click(object sender, EventArgs e)
+        {
+            string age = ageTextBox.Text;
+            string dollar = dollarTextBox.Text;
+            //removes dollar sign if user inputs
+            if (dollar.Contains('$'))
+            {
+                dollar = dollar.Remove(dollar.IndexOf('$'), 1);
+            }
+            string message = "At the ripe age of " + age + ", you should really have more than $" + dollar + " in your pocket.";
+            fortuneLabel.Text = message;
+        }
+    }
+}
